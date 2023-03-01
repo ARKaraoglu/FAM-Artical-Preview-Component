d42df7668cf80e1b5c77135b1e123656ceba29ce
Ahmet Rusen Karaoglu <87625650+ARKaraoglu@users.noreply.github.com>
2023-03-01 17:25:25 -0500

    edits

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ console.log("hello");
 
 const activeContainer = document.querySelector(".active-container");
 
+const desktopActiveContainer = document.querySelector(
+    ".desktop-active-container"
+);
+
 const shareButtons = document.querySelectorAll(".forward-image");
 
 for (let x = 0; x < shareButtons.length; x++) {
     shareButtons[x].addEventListener("click", function () {
         activeContainer.classList.toggle("display-none");
+        desktopActiveContainer.classList.toggle("display-none");
     });
 }
